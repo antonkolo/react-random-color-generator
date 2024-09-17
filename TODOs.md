0dfd95d03a3bc477e3ba410d191d899f047a354d
Anton Kolo <kvanton7@gmail.com>
2024-09-17 21:05:00 +0200

    UI & Color generation functionality implemented

---
+++ TODOs.md
@@ -0,0 +1,14 @@
+# TODOs
+
+- [ ] Finish watching a react random emoji generator video
+- [ ] Setup a project
+- [ ] Design the page layout in figma
+  - [ ] Watch a short figma tutorial
+  - [ ] Background (is going to be painted)
+  - [ ] h1 (huge) displaying the hex code
+  - [ ] Button, to generate color
+- [ ] Create the necessary react components
+  - [ ] Background: Make it change based on button click
+  - [ ] H1 Text displaying the color (make text color change based on the background color in order to keep it visible): Using state
+    - [ ] Add spinning symbols animation
+  - [ ] Button to generate color: influences the background (but is also influenced by the background)
---
+++ src/App.js
@@ -0,0 +1,83 @@
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+// styled components
+const Main = styled.main`
+  width: 100%;
+  height: 100vh;
+  background-color: #f1f3ff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-family: Danfo, sans-serif;
+  font-size: 40px;
+  max-width: 400px;
+  text-align: center;
+  margin-top: 24px;
+  margin-bottom: 0;
+`;
+
+const ColorBox = styled.div`
+  box-sizing: border-box;
+  margin-top: 24px;
+  max-width: 1060px;
+  max-height: 600px;
+  min-width: 150px; /* Minimum width before scroll or other behavior */
+  min-height: 150px; /* Minimum height before scroll or other behavior */
+  width: 100%; /* Take up to 100% of the flex parent width */
+  height: 100%;
+  background-color: ${(props) => props.$bgcolor};
+  border: 2px solid #000;
+  border-radius: 40px;
+`;
+
+const GenerateButton = styled.button`
+  outline: inherit;
+  display: inline-block;
+  font-family:
+    Madimi One,
+    sans-serif;
+
+  font-size: 28px;
+  box-sizing: border-box;
+  margin-top: 24px;
+  margin-bottom: 24px;
+  padding: 20px 80px;
+  border-radius: 20px;
+  background-color: #000;
+  color: #f1f3ff;
+  letter-spacing: 2px;
+  border: 1px solid #000;
+
+  &:hover {
+    background-color: #c2c2c2;
+    color: #f1f3ff;
+    cursor: pointer;
+  }
+
+  &:active {
+    background-color: #c2c2c2;
+    color: #f1f3ff;
+    border: #f1f3ff;
+    cursor: pointer;
+  }
+`;
+
+export default function App() {
+  // state declaration
+  const [color, setColor] = useState('#fff');
+
+  return (
+    <Main>
+      <Title>Random Color Generator</Title>
+      <ColorBox $bgcolor={color} />
+      <GenerateButton onClick={() => setColor(randomColor())}>
+        GENERATE
+      </GenerateButton>
+    </Main>
+  );
+}
